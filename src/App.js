736b0a6103c4852b456e8bd61262813dffbdae27
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-07-02 20:58:47 +0300

    Remove unneeded div

--- src/App.js
+++ src/App.js
@@ -34,54 +34,52 @@ class App extends Component {
     return (
       <div className="App">
         <div className="header">
-          <div className="header__pieces">
-            <StaggeredMotion
-              defaultStyles={headerPieces.map(headerPiece => ({...headerPiece, z: 0}))}
-              styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => 
-                i === 0
-                ? {z: slowSpring(40)}
-                : {z: slowSpring(prevInterpolatedStyles[i - 1].z)}
-              )}
-            >
-              {interpolatingStyles => (
-                <div>
-                  {interpolatingStyles.map((style, i) =>
-                    <div 
-                      key={i}
-                      className="header__piece-wrap"
-                      style={{
-                        transform: `translateZ(${style.z}px)`,
-                      }}
+          <StaggeredMotion
+            defaultStyles={headerPieces.map(headerPiece => ({...headerPiece, z: 0}))}
+            styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => 
+              i === 0
+              ? {z: slowSpring(40)}
+              : {z: slowSpring(prevInterpolatedStyles[i - 1].z)}
+            )}
+          >
+            {interpolatingStyles => (
+              <div className="header__pieces">
+                {interpolatingStyles.map((style, i) =>
+                  <div 
+                    key={i}
+                    className="header__piece-wrap"
+                    style={{
+                      transform: `translateZ(${style.z}px)`,
+                    }}
+                  >
+                    <Motion
+                      defaultStyle={{opacity: 0}}
+                      style={{opacity: slowSpring(1)}}
                     >
-                      <Motion
-                        defaultStyle={{opacity: 0}}
-                        style={{opacity: slowSpring(1)}}
-                      >
-                        {({opacity}) => (
-                          <div 
-                            className="header__piece__shadow" 
-                            style={{
-                              left: `${style.left}%`, 
-                              top: `${style.top}%`, 
-                              width: `${style.width}%`, 
-                              height: `${style.height}%`, 
-                              opacity,
-                            }}
-                          />
-                        )}
-                      </Motion>
-                      <div 
-                        className="header__piece" 
-                        style={{
-                          clipPath: `polygon(${style.left}% ${style.top}%, ${style.left + style.width}% ${style.top}%, ${style.left + style.width}% ${style.top + style.height}%, ${style.left}% ${style.top + style.height}%)`
-                        }} 
-                      />
-                    </div>
-                  )}
-                </div>
-              )}
-            </StaggeredMotion>
-          </div>
+                      {({opacity}) => (
+                        <div 
+                          className="header__piece__shadow" 
+                          style={{
+                            left: `${style.left}%`, 
+                            top: `${style.top}%`, 
+                            width: `${style.width}%`, 
+                            height: `${style.height}%`, 
+                            opacity,
+                          }}
+                        />
+                      )}
+                    </Motion>
+                    <div 
+                      className="header__piece" 
+                      style={{
+                        clipPath: `polygon(${style.left}% ${style.top}%, ${style.left + style.width}% ${style.top}%, ${style.left + style.width}% ${style.top + style.height}%, ${style.left}% ${style.top + style.height}%)`
+                      }} 
+                    />
+                  </div>
+                )}
+              </div>
+            )}
+          </StaggeredMotion>
         </div>
       </div>
     );
