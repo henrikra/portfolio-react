fedea311138c791746a17a49a75e0178989d9fb4
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-07-04 20:38:15 +0300

    Remove extra word

--- src/App.js
+++ src/App.js
@@ -206,7 +206,7 @@ class App extends Component {
               <figure className="my-picture-container">
                 <img className="my-picture" src={require('./henrik.jpg')} alt="Picture of me"/>
                 <figcaption className="my-picture__caption">
-                  Tässä vielä kuva musta niin tunnistat kun nähdään!
+                  Tässä vielä kuva niin tunnistat kun nähdään!
                 </figcaption>
               </figure>
               <div className="contacts">
