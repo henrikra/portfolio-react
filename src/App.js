63b3afc7d1cc5153f29c586459c8618f53e8c96c
Henrik Raitasola <henrik.raitasola@gmail.com>
2017-07-05 14:40:13 +0300

    Remove unused class

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div>
         <div className="header">
           {this.state.showTitle && (
             <Motion
